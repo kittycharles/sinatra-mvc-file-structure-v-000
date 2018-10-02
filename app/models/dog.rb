fe979e4230127238580eb7a2c996a9d88d72f7f7
Richard Sanchez <richard.charles.s@gmail.com>
2018-10-02 20:26:29 +0000

    automatically backed up by learn

---
+++ app/models/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+
+  @@all = []
+  
+  def initialize(name, breed, age)
+    @name = name
+    @age = age
+    @breed = breed
+    @@all << self
+  end
+
+  def all
+    @@all 
+  end
+
+end
